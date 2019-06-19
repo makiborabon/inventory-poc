982fa2a9bf784ab0ab56a1f307adb60e2f3c13b7
joy <jooyaa.327@gmail.com>
2019-06-19 20:13:12 +0800

    Loginsample

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,17 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        $guard = array_get($exception->guards(), 0);
+        switch ($guard) {
+            case 'admin':
+                $login = 'admin.login';
+                break;
+            
+            default:
+                $login  = 'login';
+                break;
+        }
+
         return parent::render($request, $exception);
     }
 }
